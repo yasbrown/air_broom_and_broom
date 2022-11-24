0c6467faf2adfbc20f548d58cee4f8b929494894
John Romero <romerojohn1012@gmail.com>
2022-11-24 14:31:45 +0000

    added map in showpage

--- app/controllers/broomsticks_controller.rb
+++ app/controllers/broomsticks_controller.rb
@@ -12,7 +12,6 @@ class BroomsticksController < ApplicationController
       @broomsticks = Broomstick.all
     end
 
-    @broomsticks = Broomstick.all
     @markers = @broomsticks.geocoded.map do |broomstick|
       {
         lat: broomstick.latitude,
@@ -23,6 +22,8 @@ class BroomsticksController < ApplicationController
 
   def show
     @booking = Booking.new
+    # @broomstick = Broomstick.find(params[:id])
+    @markers = [{ lat: @broomstick.latitude, lng: @broomstick.longitude }]
   end
 
   def new
