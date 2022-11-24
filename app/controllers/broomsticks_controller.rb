e57e2f95c29c66eb31e4097cb35dc8a6d41eab28
francisco goicoechea <frangoico97@gmail.com>
2022-11-24 16:34:21 +0000

    finished search bar

--- app/controllers/broomsticks_controller.rb
+++ app/controllers/broomsticks_controller.rb
@@ -4,7 +4,7 @@ class BroomsticksController < ApplicationController
 
   def index
     if params[:q].present?
-      @broomsticks = Broomstick.search_by_name_and_address(params[:q])
+      @broomsticks = Broomstick.search_by_name_and_address_location(params[:q])
       if @broomsticks.nil?
         @broomsticks = Broomstick.all
       end
@@ -12,7 +12,6 @@ class BroomsticksController < ApplicationController
       @broomsticks = Broomstick.all
     end
 
-    @broomsticks = Broomstick.all
     @markers = @broomsticks.geocoded.map do |broomstick|
       {
         lat: broomstick.latitude,
