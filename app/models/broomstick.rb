2e8d2785c2028c9d728ae12b35d4b2a5640ada63
Lydia Ratto <lydiaratto@gmail.com>
2022-11-25 12:58:18 +0000

    updated booking and broomstick show pages

--- app/models/broomstick.rb
+++ app/models/broomstick.rb
@@ -18,8 +18,6 @@ class Broomstick < ApplicationRecord
 
   has_many :users, through: :bookings
 
-  has_one_attached :photo
-
   include PgSearch::Model
   pg_search_scope :search_by_name_and_address_location,
   against: [ :name, :address, :latitude, :longitude],
