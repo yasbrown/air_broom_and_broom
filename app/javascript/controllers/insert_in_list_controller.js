a893a523ed3a22bcfe5a9898ec76d25105d25349
yasmin brown <yasminbrown6@hotmail.com>
2022-11-25 14:44:28 +0000

    submit reviews form fix

--- app/javascript/controllers/insert_in_list_controller.js
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -18,7 +18,9 @@ export default class extends Controller {
         if (data.inserted_item) {
           this.itemsTarget.insertAdjacentHTML(this.positionValue, data.inserted_item)
         }
-        this.formTarget.outerHTML = data.form
+        // console.log(new FormData(this.formTarget))
+        this.formTarget.outerHTML = this.formTarget.outerHTML
+
       })
   };
 };
