c048e092cd483846e926abc3d4ff4df2af30743d
francisco goicoechea <frangoico97@gmail.com>
2022-11-25 15:24:59 +0000

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,28 +8,46 @@ require "faker"
 #   Character.create(name: "Luke", movie: movies.first)
 
 puts "Deleting all broomstick records"
+Review.destroy_all
+Booking.destroy_all
 Broomstick.destroy_all
+# User.destroy_all
 puts "destroyed"
-5.times do
+1.times do
   user = User.create(
     email: Faker::Internet.email,
     username: Faker::Internet.user,
     password: Faker::Internet.password
   )
 
-  2.times do
-    broomstick = Broomstick.create(
-      name: Faker::Movies::HarryPotter.spell,
-      stability: rand(1..3),
-      speed: rand(1..3),
-      price: rand(100..1000),
-      user: user,
-      description: Faker::Lorem.paragraph,
-      address: "London"
-    )
-    puts "Broomstick with id #{broomstick.id} has been created"
-    puts "User with id #{user.id} has been created"
-  end
+  Broomstick.create(
+    name: "Harry",
+    stability: 2,
+    speed: 3,
+    price: 100,
+    user: user,
+    description: "The best one",
+    address: "124 Boulevard de la republique, france",
+    photo: "https://www.microminiatures.co.uk/wp-content/uploads/2021/06/03487.jpg"
+  )
+  Broomstick.create(
+    name: "Nancy",
+    stability: 3,
+    speed: 1,
+    price: 500,
+    user: user,
+    description: "Enjoy you trip",
+    address: "124 Boulevard de la republique, france",
+    photo: "https://images.partydelights.co.uk/PROP/22/7/front/v4/fac/4.jpg"
+  )
+  Broomstick.create(
+    name: "Tere",
+    stability: 1,
+    speed: 3,
+    price: 200,
+    user: user,
+    description: "Hello",
+    address: "124 Boulevard de la republique, france",
+    photo: "https://static9.depositphotos.com/1328797/1188/i/600/depositphotos_11882309-stock-photo-senior-woman-broom.jpg"
+  )
 end
-
-puts "Finished!"
